ac14f3ce39cbc857c004b69432c748be7af9efac
apelsinom <apelsinom@mail.ru>
2024-04-25 14:53:48 +0300

    select view WatchAnalog without size adaptation

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import './App.css';
 function App() {
     const [date, setDate] = useState(new Date());
     const [changeWatch, setChangeWatch] = useState(true);
+    const [click, setClick] = useState('Click for analog Watch');
+
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -14,11 +16,15 @@ function App() {
         return () => clearInterval(intervalId);
     }, [])
 
-    const showWatchHandler = () => setChangeWatch(!changeWatch);
+    const showWatchHandler = () => {
+        setChangeWatch(!changeWatch)
+        if (click==='Click for analog Watch') setClick('Click for electron Watch')
+        if (click==='Click for electron Watch') setClick('Click for analog Watch')
+    };
 
     return (
         <div className="App">
-            <h1 className="click" onClick={showWatchHandler}>Click me</h1>
+            <h1 className="click" onClick={showWatchHandler}>{click}</h1>
             {changeWatch ? <WatchElectron date={date}/> : <WatchAnalog date={date} />}
         </div>
     );
--- src/components/WatchAnalog/WatchAnalog.tsx
+++ src/components/WatchAnalog/WatchAnalog.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './WatchAnalog.module.css'
+import Color from './images/watchColor.png'
+import Incline from './images/watchIncline.png'
+import Kids from './images/watchKids.png'
+import Roman from './images/watchRoman.png'
+import Yellow from './images/watchYellow.png'
+import Black from './images/watchBlack.png'
 
 type Props = {
     date: Date
 }
-export const WatchAnalog = ({date}:Props) => {
+const watchView = [
+    Black, Color, Incline, Kids, Roman, Yellow
+]
+export const WatchAnalog = ({date}: Props) => {
+    const [indexWatchView, setIndexWatchView] = useState(0)
+
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
@@ -13,17 +24,28 @@ export const WatchAnalog = ({date}:Props) => {
     const minuteRotation = (minutes * 6) + (seconds / 10);
     const secondRotation = seconds * 6;
 
+    const changeWatchHandler = () => {
+        if (indexWatchView===watchView.length-1) {
+            setIndexWatchView(0)
+        } else {
+            setIndexWatchView(indexWatchView+1)
+        }
+    }
+
     return (
-        <div className={s.clock}>
-            <div className={s.hour}>
-                <div className={s.hours} style={{transform: `rotate(${hourRotation}deg)`}}></div>
+        <>
+            <div className={s.clock} style={{background: `url(${watchView[indexWatchView]}) center / cover`}}>
+                <div className={s.hour}>
+                    <div className={s.hours} style={{transform: `rotate(${hourRotation}deg)`}}></div>
+                </div>
+                <div className={s.minute}>
+                    <div className={s.minutes} style={{transform: `rotate(${minuteRotation}deg)`}}></div>
+                </div>
+                <div className={s.second}>
+                    <div className={s.seconds} style={{transform: `rotate(${secondRotation}deg)`}}></div>
+                </div>
             </div>
-            <div className={s.minute}>
-                <div className={s.minutes} style={{transform: `rotate(${minuteRotation}deg)`}}></div>
-            </div>
-            <div className={s.second}>
-                <div className={s.seconds} style={{transform: `rotate(${secondRotation}deg)`}}></div>
-            </div>
-        </div>
+            <h1 className={s.click} onClick={changeWatchHandler}>Change watch face</h1>
+        </>
     );
 };
